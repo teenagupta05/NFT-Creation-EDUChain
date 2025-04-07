960a3e61bc3000d89aeae3b216c86b1c1270348a
teenagupta05 <teena6224@gmail.com>
2025-04-07 12:31:12 +0530

    Update project.sol

--- contract/project.sol
+++ contract/project.sol
@@ -1,1 +1,88 @@
+/ SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleNFT {
+    string public name = "MyNFTCollection";
+    string public symbol = "MNFT";
+
+    uint256 private _tokenIds;
+
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => string) private _tokenURIs;
+    mapping(uint256 => address) private _tokenApprovals;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract owner can perform this action");
+        _;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        require(_owner != address(0), "Invalid address");
+        return _balances[_owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address tokenOwner = _owners[tokenId];
+        require(tokenOwner != address(0), "Token does not exist");
+        return tokenOwner;
+    }
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_owners[tokenId] != address(0), "Token does not exist");
+        return _tokenURIs[tokenId];
+    }
+
+    function createNFT(address recipient, string memory uri) public onlyOwner returns (uint256) {
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+
+        _owners[newTokenId] = recipient;
+        _balances[recipient]++;
+        _tokenURIs[newTokenId] = uri;
+
+        emit Transfer(address(0), recipient, newTokenId);
+
+        return newTokenId;
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        address tokenOwner = ownerOf(tokenId);
+        require(msg.sender == tokenOwner, "Caller is not the token owner");
+
+        _tokenApprovals[tokenId] = to;
+
+        emit Approval(tokenOwner, to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(_owners[tokenId] != address(0), "Token does not exist");
+        return _tokenApprovals[tokenId];
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        address tokenOwner = ownerOf(tokenId);
+        require(tokenOwner == from, "Incorrect owner");
+        require(msg.sender == from || msg.sender == getApproved(tokenId), "Not authorized");
+
+        _owners[tokenId] = to;
+        _balances[from]--;
+        _balances[to]++;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    function totalMinted() public view returns (uint256) {
+        return _tokenIds;
+    }
+}
 
